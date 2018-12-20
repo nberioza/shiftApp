24c72d41571721d5d9b2c7046f185d4b60398ce9
nberioza <33886766+nberioza@users.noreply.github.com>
2018-12-20 17:54:48 +0200

    Update Firebase.js

--- src/Components/Common/Firebase.js
+++ src/Components/Common/Firebase.js
@@ -5,12 +5,7 @@
 const Firebase=()=>{
 const firebase = require('firebase') ;
     const config = {
-        apiKey: 'AIzaSyBl_yXrVKzbp6bQ0aJ6kNZqoRF0Sj-WJmE',
-        authDomain: 'shiftsapp-1cdc1.firebaseapp.com',
-        databaseURL: 'https://shiftsapp-1cdc1.firebaseio.com',
-        projectId: 'shiftsapp-1cdc1',
-        storageBucket: '',
-        messagingSenderId: '66390814105'
+      //your firebase config goes here
       };
     return  firebase.initializeApp(config);
 
